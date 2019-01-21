72a5d124b046582ea9115066193c73e8a6f923ec
Győri Sándor <alexaegis@gmail.com>
2019-01-21 11:18:03 +0100

    tsify

--- e2e/protractor.conf.ts
+++ e2e/protractor.conf.ts
@@ -1,9 +1,9 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-reporter');
+import { SpecReporter } from 'jasmine-spec-reporter';
 
-exports.config = {
+export const config = {
 	allScriptsTimeout: 11000,
 	specs: ['./src/**/*.e2e-spec.ts'],
 	capabilities: {
--- src/karma.conf.ts
+++ src/karma.conf.ts
@@ -1,17 +1,19 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function(config) {
+const plugins: Array<any> = [
+	require('karma-jasmine'),
+	require('karma-chrome-launcher'),
+	require('karma-jasmine-html-reporter'),
+	require('karma-coverage-istanbul-reporter'),
+	require('@angular-devkit/build-angular/plugins/karma')
+];
+
+module.exports = config => {
 	config.set({
 		basePath: '',
 		frameworks: ['jasmine', '@angular-devkit/build-angular'],
-		plugins: [
-			require('karma-jasmine'),
-			require('karma-chrome-launcher'),
-			require('karma-jasmine-html-reporter'),
-			require('karma-coverage-istanbul-reporter'),
-			require('@angular-devkit/build-angular/plugins/karma')
-		],
+		plugins: plugins,
 		client: {
 			clearContext: false // leave Jasmine Spec Runner output visible in browser
 		},
