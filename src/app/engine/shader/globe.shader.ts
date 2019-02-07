82feff407a7ecd3ab033eb0f7e2243035c7df3fd
Győri Sándor <alexaegis@gmail.com>
2019-02-07 20:33:48 +0100

    fix texture location

--- src/app/engine/shader/globe.shader.ts
+++ src/app/engine/shader/globe.shader.ts
@@ -2,7 +2,7 @@ import { Shader } from 'three';
 import * as THREE from 'three';
 
 export const globeShader: Shader = {
-	uniforms: { texture: { value: new THREE.TextureLoader().load(`../../assets/world.jpg`) } },
+	uniforms: { texture: { value: new THREE.TextureLoader().load(`assets/world.jpg`) } },
 	vertexShader: `
 		varying vec3 vNormal;
 		varying vec2 vUv;
