9c5cd56b73e17e720b2a92955bf1643f207c051c
Győri Sándor <alexaegis@gmail.com>
2019-02-11 23:32:44 +0100

    added resizeobserver

--- src/app/component/timeline/timeline.component.ts
+++ src/app/component/timeline/timeline.component.ts
@@ -1,22 +1,37 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { MediaObserver } from '@angular/flex-layout';
+import { Component, OnInit, HostListener, HostBinding, AfterViewInit, Input, ElementRef } from '@angular/core';
 import * as moment from 'moment';
 import { Moment } from 'moment';
+import { mixinTabIndex } from '@angular/material';
+import ResizeObserver from 'resize-observer-polyfill';
 
 @Component({
 	selector: 'app-timeline',
 	templateUrl: './timeline.component.html',
 	styleUrls: ['./timeline.component.scss']
 })
-export class TimelineComponent implements OnInit {
-	beginning: Moment;
-	frame: Moment;
-	unitsBetween: number;
-
-	constructor() {
+export class TimelineComponent implements OnInit, AfterViewInit {
+	constructor(public el: ElementRef) {
 		this.frame = moment(0);
 		this.frame.set('months', 1);
 		this.beginning = moment(0);
-		this.unitsBetween = this.calcUnitsBetween();
+		this.calcUnitsBetween();
+	}
+	beginning: Moment;
+	frame: Moment;
+	unitsBetween: number;
+	distanceBetweenUnits: number;
+	width: number;
+	ngAfterViewInit(): void {
+		// ResizeObserver is not really supportod outside of chrome.
+		// It can also make the app crash on MacOS https://github.com/que-etc/resize-observer-polyfill/issues/36
+		const resize$ = new ResizeObserver(e => {
+			e.forEach(change => {
+				this.width = change.contentRect.width;
+				this.distanceBetweenUnits = this.width / this.unitsBetween;
+			});
+		});
+		resize$.observe(this.el.nativeElement);
 	}
 
 	/**
@@ -31,12 +46,13 @@ export class TimelineComponent implements OnInit {
 		if ((this.unitsBetween > 5 && $event.deltaY < 0) || ($event.deltaY > 0 && this.unitsBetween < 31)) {
 			this.frame.add(this.normalize($event.deltaY), 'day');
 			this.beginning.subtract(this.normalize($event.deltaY), 'day');
-			this.unitsBetween = this.calcUnitsBetween();
+			this.calcUnitsBetween();
 		}
 	}
 
-	calcUnitsBetween(): number {
-		return this.frame.diff(this.beginning, 'day');
+	calcUnitsBetween(): void {
+		this.unitsBetween = this.frame.diff(this.beginning, 'day');
+		this.distanceBetweenUnits = this.width / this.unitsBetween;
 	}
 
 	ngOnInit() {}
--- src/app/directive/repeat.directive.ts
+++ src/app/directive/repeat.directive.ts
@@ -2,12 +2,18 @@ import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({ selector: '[appRepeat]' })
 export class RepeatDirective {
+	c = 0;
 	constructor(private templateRef: TemplateRef<any>, private viewContainer: ViewContainerRef) {}
 
 	@Input('appRepeat') set count(c: number) {
-		this.viewContainer.clear();
-		for (let i = 0; i < c; i++) {
-			this.viewContainer.createEmbeddedView(this.templateRef);
+		for (let i = 0; i < Math.abs(this.c - c); i++) {
+			if (c < this.c) {
+				this.viewContainer.detach();
+			} else if (c > this.c) {
+				this.viewContainer.createEmbeddedView(this.templateRef);
+			}
 		}
+
+		this.c = c;
 	}
 }
