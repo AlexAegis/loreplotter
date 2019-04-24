68f3bf191e048ca82a5b41ac73ff494413cf1937
Győri Sándor <alexaegis@gmail.com>
2019-04-24 02:47:51 +0200

    color adjust, reenabled subdividers, removed unneessary shader

--- src/app/engine/object/atmosphere.class.ts
+++ src/app/engine/object/atmosphere.class.ts
@@ -1,25 +1,9 @@
 import { Basic } from './basic.class';
-import { Sun } from './sun.class';
-import { Planet } from './../../model/planet.class';
 import * as THREE from 'three';
-import { Object3D, ShaderMaterialParameters, SphereBufferGeometry, ShaderMaterial } from 'three';
-import { atmosphereShader } from '../shader/atmosphere.shader';
+import { SphereBufferGeometry } from 'three';
 import { Globe } from './globe.class';
-import * as dat from 'dat.gui';
 
 export class Atmosphere extends Basic {
-	public planet_scene: THREE.Scene;
-	public planet_camera: THREE.PerspectiveCamera;
-	public planet_texture: THREE.WebGLRenderTarget;
-	public base_directions: Array<THREE.Vector3>;
-	public directions: Array<THREE.Vector3>;
-	public rayleigh_length: number;
-	public rayleigh_constant: number;
-	public mie_length: number;
-	public mie_constant: number;
-	public radius: number;
-	public uniforms: any;
-	public attributes: any;
 	public mesh: THREE.Mesh;
 
 	public scene: THREE.Scene;
@@ -31,8 +15,6 @@ export class Atmosphere extends Basic {
 		super();
 
 		this.geometry = new SphereBufferGeometry(planet.radius * 1.1, 80, 80);
-
-		const gui = new dat.GUI();
 
 		this.material = new THREE.MeshPhysicalMaterial({
 			color: '#6266ff',
